7ea989d74d2a73842c0632fa157942d193000280
Chaiwat Apithanawit <chaiwatappi@gmail.com>
2021-04-17 20:43:47 +0700

    add hemework3

--- homework3/src/app/app.module.ts
+++ homework3/src/app/app.module.ts
@@ -2,26 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CountingComponent } from './component/counting/counting.component';
-import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
-
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CountingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatCardModule,
-    MatIconModule
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- readme.txt
+++ readme.txt
@@ -2,3 +2,4 @@
 แบบฝึกหัดข้อ 11 -> folder Lab4
 Homework 1 -> folder homework1
 Homework 2 และ 4 -> folder homework2
+Homework 3 -> folder homework3
